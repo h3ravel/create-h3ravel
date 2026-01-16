d2cd53e0436b0ed292c0e93b6e30d7f59abccddd
3m1n3nc3 <mygames.ng@gmail.com>
2026-01-16 23:42:31 +0100

    feat: update dependencies and enhance template options

--- src/templates.ts
+++ src/templates.ts
@@ -15,30 +15,35 @@ export const templates = [
         name: 'Full Starter Kit',
         alias: 'full',
         hint: 'A full H3ravel application with everything possible',
-        source: 'github:h3ravel/h3ravel',
+        source: 'github:h3ravel/h3ravel#77e2ff2',
+        prereleaseSource: 'github:h3ravel/h3ravel#2eec534',
     },
     {
         name: 'Lean Starter Kit',
         alias: 'lean',
         hint: 'A lean H3ravel application with just the framework core',
-        source: null,
+        source: undefined,
+        prereleaseSource: undefined
     },
     {
         name: 'API Starter Kit',
         alias: 'api',
         hint: 'Creates a H3ravel application for building JSON APIs',
-        source: null
+        source: undefined,
+        prereleaseSource: undefined
     },
     {
         name: 'Web Starter Kit',
         alias: 'web',
         hint: 'Creates a H3ravel application for building a server rendered app',
-        source: null
+        source: undefined,
+        prereleaseSource: undefined
     },
     {
         name: 'Inertia Starter Kit',
         alias: 'inertia',
         hint: 'Inertia application with a frontend framework of your choice',
-        source: null
+        source: undefined,
+        prereleaseSource: undefined
     },
 ]
